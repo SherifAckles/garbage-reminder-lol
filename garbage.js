6caff629d79acb2fb6a249751f9b6e1151f44c8c
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-09-29 13:17:06 -0500

    enhance js, css, remove colsole.log

--- garbage.js
+++ garbage.js
@@ -1,24 +1,32 @@
 const weekday = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-  ];
-  
-  const garbageDays = ['Saturday', 'Tuesday', 'Thursday'];
-  const garbageOut = new Date();
-  let today = weekday[garbageOut.getDay()];
-  
-    if (
-      today === garbageDays[0] ||
-      today === garbageDays[1] ||
-      today === garbageDays[2]
-    ) {
-      console.log(`today at 7 AM you have to get the garbage out`);
-    }
-    else{
-      console.log("...No garbage to take out today... RELAX mate..." ,"\n", "\n", "...Hoy no hay basura que sacar... RELAJATE compañero...")
-    }
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const garbageDays = ["Saturday", "Tuesday", "Thursday"];
+const garbageOut = new Date();
+let today = weekday[garbageOut.getDay()];
+document.querySelector(".message1").textContent = today;
+document.querySelector(".message1").style.color = "blue";
+if (
+  today === garbageDays[0] ||
+  today === garbageDays[1] ||
+  today === garbageDays[2]
+) {
+  document.querySelector(
+    ".message2"
+  ).textContent = `today at 7 AM you have to get the garbage out`;
+  document.querySelector(".message2").style.color = "green";
+} else {
+  (document.querySelector(".message2").textContent =
+    "...No garbage to take out today... RELAX mate..."),
+    "\n",
+    "\n",
+    "...Hoy no hay basura que sacar... RELAJATE compañero...";
+  document.querySelector(".message2").style.color = "grey";
+}
