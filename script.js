d437e4750c630bfc8c5133fa900a2619e886ec69
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-10-09 16:05:14 -0500

    add bootstarp

--- script.js
+++ script.js
@@ -16,14 +16,13 @@ message1.textContent = today;
 message1.style.color = "blue";
 // using for loop instead of assiging array positions
 for (let i = 0; i < garbageDays.length; i++) {
-  console.log(garbageDays[i]);
   if (today === garbageDays[i]) {
     message1.textContent = `today at 7 AM you have to get the garbage out`;
     message1.style.color = "green";
   } else {
-    (message2.textContent = "...No garbage to take out today... RELAX mate..."),
+    (message2.textContent = "No garbage to take out today"),
       "\n",
-      "...Hoy no hay basura que sacar... RELAJATE compañero...";
+      "Hoy no hay basura que sacar";
     message2.style.color = "grey";
   }
 }
