3feca713b4b1e898ee5630099030daf663ba7f97
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-10-09 15:00:12 -0500

    remove spaces between lines

--- script.js
+++ script.js
@@ -7,25 +7,21 @@ const weekday = [
   "Friday",
   "Saturday",
 ];
-
 const garbageDays = ["Saturday", "Tuesday", "Thursday"];
 const garbageOut = new Date();
 const today = weekday[garbageOut.getDay()];
 const message2 = document.querySelector(".message2");
 const message1 = document.querySelector(".message1");
-
 message1.textContent = today;
 message1.style.color = "blue";
 // using for loop instead of assiging array positions
 for (let i = 0; i < garbageDays.length; i++) {
   console.log(garbageDays[i]);
-
   if (today === garbageDays[i]) {
     message1.textContent = `today at 7 AM you have to get the garbage out`;
     message1.style.color = "green";
   } else {
     (message2.textContent = "...No garbage to take out today... RELAX mate..."),
-      "\n",
       "\n",
       "...Hoy no hay basura que sacar... RELAJATE compañero...";
     message2.style.color = "grey";
