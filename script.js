5faab35caf93a20d08bc23973a7a74bede3d1ab8
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-10-09 22:02:32 -0500

    refactor all codes

--- script.js
+++ script.js
@@ -14,9 +14,13 @@ const message2 = document.querySelector(".message2");
 const message1 = document.querySelector(".message1");
 message1.textContent = today;
 message1.style.color = "blue";
-// using for loop instead of assiging array positions
-for (let i = 0; i < garbageDays.length; i++) {
-  if (today === garbageDays[i]) {
+// using for of loop instead of assiging array positions
+for (const i of garbageDays) {
+  // we can use one of these (ifs) to check if today is the garbageday to take out
+  //if (garbageDays.includes(today)) {
+    // }
+    
+  if (today === garbageDays) {
     message1.textContent = `today at 7 AM you have to get the garbage out`;
     message1.style.color = "green";
   } else {
